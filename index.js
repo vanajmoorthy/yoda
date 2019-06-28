817e990c435ed66d46e2335ff44f1f6aea5a7581
Vanaj Moorthy <vanajmoorthy@gmail.com>
2019-06-28 17:24:14 +0530

    Trying to do xml

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
-var unirest = require("unirest");
+function sendRequest() {
+	var soap = require("soap");
+	var url = "http://www.yodaspeak.co.uk/webservice/yodatalk.php?wsdl";
+	var args = { name: "value" };
+	soap.createClient(url, function(err, client) {
+		client.request(args, function(err, result) {
+			console.log(result);
+		});
+	});
 
-unirest.post("https://yodish.p.rapidapi.com/yoda.json?text=Master+Obiwan+has+lost+a+planet.")
-.header("X-RapidAPI-Host", "yodish.p.rapidapi.com")
-.header("X-RapidAPI-Key", "44febd3629msh1bdc68bd2d88771p1ade31jsn4ae882570bbe")
-.header("Content-Type", "application/x-www-form-urlencoded")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
+
+	// var token = document.getElementsByClassName("input-box")[0].value;
+
+	// var URLString = "https://yodish.p.rapidapi.com/yoda.json?text=";
+
+	// var enc = encodeURIComponent(token);
+
+	// var fullURL = URLString + enc;
+
+	// console.log(fullURL);
+
+	// var request = new XMLHttpRequest();
+	// request.open("POST", fullURL, true);
+
+	// request.setRequestHeader("X-RapidAPI-Host", "yodish.p.rapidapi.com");
+	// request.setRequestHeader(
+	// 	"X-RapidAPI-Key",
+	// 	"44febd3629msh1bdc68bd2d88771p1ade31jsn4ae882570bbe"
+	// );
+	// request.setRequestHeader(
+	// 	"Content-Type",
+	// 	"application/x-www-form-urlencoded"
+	// );
+
+	// request.onload = function() {
+	// 	// Begin accessing JSON data here
+	// 	var data = JSON.parse(this.response);
+
+	// 	if (request.status >= 200 && request.status < 400) {
+	// 		console.log(data.contents.translated);
+	// 	} else {
+	// 		console.log("error");
+	// 	}
+	// };
+	// request.send();
+}
--- stars.js
+++ stars.js
@@ -1,42 +1,107 @@
-window.onload = function() {
-	const colors = ["#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"];
+let balls = [];
 
-	const numBalls = 300;
-	const balls = [];
+let threshold = 30;
+let accChangeX = 0;
+let accChangeY = 0;
+let accChangeT = 0;
 
-	for (let i = 0; i < numBalls; i++) {
-		let ball = document.createElement("div");
-		ball.classList.add("ball");
-		ball.style.background =
-			colors[Math.floor(Math.random() * colors.length)];
-		ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-		ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-		ball.style.transform = `scale(${Math.random()})`;
-		ball.style.width = `${Math.random() - 0.55}em`;
-		ball.style.height = ball.style.width;
+function setup() {
+	var canvas = createCanvas(windowWidth, windowHeight);
+	canvas.parent("sketch-div");
 
-		balls.push(ball);
-		document.body.append(ball);
+	for (let i = 0; i < 99; i++) {
+		balls.push(new Ball());
 	}
-	// Keyframes
-	balls.forEach((el, i, ra) => {
-		let to = {
-			x: Math.random() * (i % 2 === 0 ? -11 : 11),
-			y: Math.random() * 12
-		};
+}
 
-		let anim = el.animate(
-			[
-				{ transform: "translate(0, 0)" },
-				{ transform: `translate(${to.x}rem, ${to.y}rem)` }
-			],
-			{
-				duration: (Math.random() + 1) * 4000, // random duration
-				direction: "alternate",
-				fill: "both",
-				iterations: Infinity,
-				easing: "ease-in-out"
-			}
-		);
-	});
-};
+function draw() {
+	background(0);
+
+	for (let i = 0; i < balls.length; i++) {
+		balls[i].move();
+		balls[i].display();
+	}
+
+	checkForShake();
+}
+
+function checkForShake() {
+	accChangeX = abs(accelerationX - pAccelerationX); // Total change in accelerationX and accelerationY
+	accChangeY = abs(accelerationY - pAccelerationY);
+	accChangeT = accChangeX + accChangeY;
+	
+	if (accChangeT >= threshold) { // If isShake
+		for (let i = 0; i < balls.length; i++) {
+			balls[i].shake();
+			balls[i].turn();
+		}
+	}
+	// If isn'tShake
+	else {
+		for (let i = 0; i < balls.length; i++) {
+			balls[i].stopShake();
+			balls[i].turn();
+			balls[i].move();
+		}
+	}
+}
+
+// This ball class
+class Ball {
+	constructor() {
+		this.x = random(width);
+		this.y = random(height);
+		this.diameter = random(2, 7);
+		this.xspeed = random(-0.3, 0.3);
+		this.yspeed = random(-0.3, 0.3);
+		this.oxspeed = this.xspeed;
+		this.oyspeed = this.yspeed;
+		this.direction = 0.7;
+	}
+
+	move() {
+		this.x += this.xspeed * this.direction;
+		this.y += this.yspeed * this.direction;
+	}
+
+	// Bounce when touch edge
+	turn() {
+		if (this.x < 0) {
+			this.x = 0;
+			this.direction = -this.direction;
+		} else if (this.y < 0) {
+			this.y = 0;
+			this.direction = -this.direction;
+		} else if (this.x > width - 20) {
+			this.x = width - 20;
+			this.direction = -this.direction;
+		} else if (this.y > height - 20) {
+			this.y = height - 20;
+			this.direction = -this.direction;
+		}
+	}
+
+	// Add to xspeed and yspeed based on change in accelerationX
+	shake() {
+		this.xspeed += random(5, accChangeX / 3);
+		this.yspeed += random(5, accChangeX / 3);
+	}
+
+	// Slow down
+	stopShake() {
+		if (this.xspeed > this.oxspeed) {
+			this.xspeed -= 0.6;
+		} else {
+			this.xspeed = this.oxspeed;
+		}
+		if (this.yspeed > this.oyspeed) {
+			this.yspeed -= 0.6;
+		} else {
+			this.yspeed = this.oyspeed;
+		}
+	}
+
+	display() {
+		ellipse(this.x, this.y, this.diameter, this.diameter);
+	}
+}
