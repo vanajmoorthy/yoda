338c4e53b4927705246269b0bd1d97d29e24f2b5
Vanaj Moorthy <vanajmoorthy@gmail.com>
2019-09-07 22:28:41 +0530

    madechanges i guess

--- index.js
+++ index.js
@@ -1,46 +1,22 @@
-function sendRequest() {
-	var soap = require("soap");
-	var url = "http://www.yodaspeak.co.uk/webservice/yodatalk.php?wsdl";
-	var args = { name: "value" };
-	soap.createClient(url, function(err, client) {
-		client.request(args, function(err, result) {
-			console.log(result);
-		});
-	});
+function yodify() {
+	var request = new XMLHttpRequest();
 
+	var input = document.getElementById("text").value;
+	var encoded = encodeURIComponent(input);
+	var url = "https://api.funtranslations.com/translate/yoda.json?text=";
+	var toSend = url + encoded;
 
-	// var token = document.getElementsByClassName("input-box")[0].value;
+	request.open(
+		"GET",
+		toSend,
+		true
+	);
 
-	// var URLString = "https://yodish.p.rapidapi.com/yoda.json?text=";
+	request.onload = function() {
+		var data = JSON.parse(this.response);
 
-	// var enc = encodeURIComponent(token);
+		document.getElementById("output").innerText = data.contents.translated;
+	};
 
-	// var fullURL = URLString + enc;
-
-	// console.log(fullURL);
-
-	// var request = new XMLHttpRequest();
-	// request.open("POST", fullURL, true);
-
-	// request.setRequestHeader("X-RapidAPI-Host", "yodish.p.rapidapi.com");
-	// request.setRequestHeader(
-	// 	"X-RapidAPI-Key",
-	// 	"44febd3629msh1bdc68bd2d88771p1ade31jsn4ae882570bbe"
-	// );
-	// request.setRequestHeader(
-	// 	"Content-Type",
-	// 	"application/x-www-form-urlencoded"
-	// );
-
-	// request.onload = function() {
-	// 	// Begin accessing JSON data here
-	// 	var data = JSON.parse(this.response);
-
-	// 	if (request.status >= 200 && request.status < 400) {
-	// 		console.log(data.contents.translated);
-	// 	} else {
-	// 		console.log("error");
-	// 	}
-	// };
-	// request.send();
-}
+	request.send();
+};
